4e65cae272fd42a53b17beea906cec9f97c44860
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-01 23:25:12 -0400

    removed https middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
 //        custom middleware
-        \App\Http\Middleware\HttpsProtocal::class,
+//        \App\Http\Middleware\HttpsProtocal::class,
     ];
 
     /**
