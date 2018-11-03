e165b87ab3c1e8246dc8ab2289843354d85b53f5
santius0 <santius1995@gmail.com>
2018-11-03 10:23:51 -0400

    lofi images

--- resources/views/content/prototype_overview.blade.php
+++ resources/views/content/prototype_overview.blade.php
@@ -11,15 +11,15 @@
     <div class="text-center">
         <div class="row">
             <div class="flex-column">
-                <img src="{{ asset('images/lofi_1.jpg') }}" title='LoFi_1'>
-                <img src="{{ asset('images/lofi2.jpg') }}" title='LoFi_2'>
-                <img src="{{ asset('images/lofi3.jpg') }}" title='LoFi_3'>
-                <img src="{{ asset('images/lofi4.jpg') }}" title='LoFi_4'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_1.jpg') }}" title='LoFi_1'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_2.jpg') }}" title='LoFi_2'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_3.jpg') }}" title='LoFi_3'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_4.jpg') }}" title='LoFi_4'>
             </div>
             <div class="flex-column">
-                <img src="{{ asset('images/prototypes/lofi/lofi5.jpg') }}" title='LoFi_5'>
-                <img src="{{ asset('images/prototypes/lofi/lofi6.jpg') }}" title='LoFi_6'>
-                <img src="{{ asset('images/prototypes/lofi/lofi7.jpg') }}" title='LoFi_7'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_5.jpg') }}" title='LoFi_5'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_6.jpg') }}" title='LoFi_6'>
+                <img src="{{ asset('images/prototypes/lofi/lofi_7.jpg') }}" title='LoFi_7'>
             </div>
         </div>
     </div>
