0614cd8d91758c1ddd172022a2da4c0a408b1ea8
santius0 <santius1995@gmail.com>
2018-11-03 10:00:01 -0400

    lofi images

--- resources/views/content/prototype_overview.blade.php
+++ resources/views/content/prototype_overview.blade.php
@@ -2,5 +2,20 @@
 @title('Reflection and LoFi Prototype')
 
 @section('post_body')
-{!! $reflection_thread->body() !!}
+    {!! $reflection_thread->body() !!}
+    <h1>Final LoFi Prototype</h1>
+    <hr>
+    <div class="row">
+        <div class="flex-column">
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+        </div>
+        <div class="flex-column">
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+            <img src="{{ asset('images/My_UWI_Owl.png') }}" title={{ env('APP_NAME') }}>
+        </div>
+    </div>
 @endsection
