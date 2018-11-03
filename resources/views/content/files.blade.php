3f8c4f4a901f0e628c5962a619698d9d504892a9
santius0 <santius1995@gmail.com>
2018-11-03 18:29:23 -0400

    added css to files

--- resources/views/content/files.blade.php
+++ resources/views/content/files.blade.php
@@ -6,8 +6,19 @@
     <h3>All files created during this project can be found here</h3>
     <br><hr>
     @foreach($files as $file)
-        <ul>
-            <li><a href="{{ $file['link'] }}">{{$file['name']}}</a> </li>
+        <ul class="docs-list">
+            <li>
+                <div class="img">
+                  <span class="fa-stack fa-lg">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa fa-user fa-stack-1x fa-inverse"></i>
+                  </span>
+                </div>
+                <div class="desc">
+                    <strong> {{ isset($file['name']) ? $file['name'] : 'File Name' }} </strong>
+                    <small> {{ isset($file['description']) ? $file['description'] : '' }}</small>
+                </div>
+            </li>
         </ul>
     @endforeach
 @endsection
