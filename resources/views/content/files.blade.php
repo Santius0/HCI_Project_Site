a93e7c7cbc930f83d8ca4efddce53da6bf2bfd34
santius0 <santius1995@gmail.com>
2018-11-03 18:41:55 -0400

    added css to files

--- resources/views/content/files.blade.php
+++ resources/views/content/files.blade.php
@@ -5,7 +5,7 @@
     <br/>
     <h3>All files created during this project can be found here</h3>
     <br><hr>
-    <div class="w3-container">
+    <div class="w3-container" id="docs-list">
         <ul id="myUL" class="w3-margin-top w3-margin-bottom">
             @foreach($files as $file)
                 <li><a href="{{ $file['link'] }}"> {{$file['name']}} </a> </li>
