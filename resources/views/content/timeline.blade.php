e63ba397309749cb5bbf82c508dc62acd921f5bc
santius0 <santius1995@gmail.com>
2018-11-03 13:29:18 -0400

    timeline templates created

--- resources/views/content/timeline.blade.php
+++ resources/views/content/timeline.blade.php
@@ -6,15 +6,11 @@
     <h1>{{ count($timeline_items) }}</h1>
     <ul class="timeline">
         @foreach($timeline_items as $item)
-            {{--@include('content._timeline_badge',--}}
-            {{--['invert' => $invert, 'badge_header' => isset($item['badge_header'])?$item['badge_header']:'Badge Header', 'badge_header_route' => isset($item['badge_header_route'])?$item['badge_header_route']:'#',--}}
-            {{--'badge_content' => $item['badge_content'], 'file_list' => isset($item['file_list'])?$item['file_list']:null, 'date' => isset($item['date'])?$item['date']:null--}}
-            {{--]--}}
-            {{--) --}}
-            @include('content._timeline_badge',
-                      ['invert' => $invert, 'badge_header' => 'Badge Header', 'badge_header_route' => 'prototype.timeline',
-                      'badge_content' => 'jjj', 'file_list' => null, 'date' => null
-                       ]
+            @include('content._timeline_badge',['invert' => $invert, 'badge_header' => isset($item['badge_header'])?$item['badge_header']:'Badge Header',
+                                                'badge_header_route' => isset($item['badge_header_route'])?$item['badge_header_route']:'#',
+                                                'badge_content' => $item['badge_content'], 'file_list' => isset($item['file_list'])?$item['file_list']:null,
+                                                'date' => isset($item['date'])?$item['date']:null
+                                                ]
             )
             @if($invert)
                 @php($invert = false)
