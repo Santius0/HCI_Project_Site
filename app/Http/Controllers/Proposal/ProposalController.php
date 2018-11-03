d80d6a23b19c1e40368187fcd51990f6b3daa573
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-02 20:34:11 -0400

    adding content

--- app/Http/Controllers/Proposal/ProposalController.php
+++ app/Http/Controllers/Proposal/ProposalController.php
@@ -15,6 +15,24 @@ class ProposalController extends Controller
 
     public function definition()
     {
+        $problem_thread = Thread::where('title', 'Project Definition')->first();
+        return view('proposal.problem_definition', compact('problem_thread'));
+    }
+
+    public function users()
+    {
+        $problem_thread = Thread::where('title', 'Problem Definition')->first();
+        return view('proposal.problem_definition', compact('problem_thread'));
+    }
+
+    public function research()
+    {
+        $problem_thread = Thread::where('title', 'Problem Definition')->first();
+        return view('proposal.problem_definition', compact('problem_thread'));
+    }
+
+    public function prototyping()
+    {
         $problem_thread = Thread::where('title', 'Problem Definition')->first();
         return view('proposal.problem_definition', compact('problem_thread'));
     }
