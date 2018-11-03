0ac51f9888e49b21e496bf46698f1411402d4e21
santius0 <santius1995@gmail.com>
2018-11-03 14:43:48 -0400

    timeline templates created

--- resources/views/_partials/_image_modal.blade.php
+++ resources/views/_partials/_image_modal.blade.php
@@ -1,9 +1,24 @@
-<!-- Wrapper for slides -->
-<div class="carousel-inner">
-    <div class="item active">
-        <img class="img-responsive" src="{{ isset($image['asset_src']) ? asset($image['asset_src']) : $image['src'] }}" alt="{{ $image['name'] }}">
-        <div class="carousel-caption">
-            {{ $image['name'] }}
+<div class="modal bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img class="img-responsive" src="{{ isset($image['asset_src']) ? asset($image['asset_src']) : $image['src'] }}" alt="{{ $image['name'] }}">
+                        <div class="carousel-caption">
+                            {{ $image['name'] }}
+                        </div>
+                    </div>
+                </div>
+            <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+            </div>
         </div>
     </div>
 </div>
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -11,7 +11,7 @@
     <title>{{ isset($title) ? $title.' | ' : '' }} {{ config('app.name') }}</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">--}}
     {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js">--}}
     {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js">--}}
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
