5a0d40bec770400ed775eed58fd72fbb238718cb
santius0 <santius1995@gmail.com>
2018-11-03 13:51:15 -0400

    timeline templates created

--- resources/views/content/_timeline_badge.blade.php
+++ resources/views/content/_timeline_badge.blade.php
@@ -40,9 +40,6 @@
 </div>
 @if(!$invert)
     <li>
-@else
-    <li class="timeline-inverted">
-@endif
     <div class="timeline-badge">
             <a><i class="fa fa-circle" id=""></i></a>
         </div>
@@ -71,3 +68,34 @@
             </div>
         </div>
     </li>
+@else
+    <li class="timeline-inverted">
+        <div class="timeline-badge">
+            <a><i class="fa fa-circle" id=""></i></a>
+        </div>
+        <div class="timeline-panel">
+            <div class="timeline-heading">
+                <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
+            </div>
+            <div class="timeline-body">
+                <p> {{ $badge_content }}</p>
+                @if(isset($file_list))
+                    <div class="files">
+                        <ul class="file-list">
+                            @foreach($file_list as $file)
+                                <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                {{--@if(isset($images))--}}
+                <button class="btn btn-success" data-toggle="modal" data-taeget=".bs-example-modal-lg">See Images</button>
+                {{--@endif--}}
+                <br>
+            </div>
+            <div class="timeline-footer">
+                {{ isset($date) ? $date : '' }}
+            </div>
+        </div>
+    </li>
+@endif
