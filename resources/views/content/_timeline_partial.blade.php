8ae7cd3dfe4184d2277140110aed49d1eb4dfb96
santius0 <santius1995@gmail.com>
2018-11-03 13:08:59 -0400

    timeline templates created

--- resources/views/content/_timeline_partial.blade.php
+++ resources/views/content/_timeline_partial.blade.php
@@ -4,7 +4,7 @@
 <ul class="timeline">
     @foreach($timeline_items as $item)
         @include('content._timeline_badge',
-                  ['invert' => $invert, 'badge_header' => $item['badge_header'], 'badge_header_route' => isset($item['badge_header_route'])?$item['badge_header_route']:'#',
+                  ['invert' => $invert, 'badge_header' => isset($item['badge_header'])?$item['badge_header']:'Badge Header', 'badge_header_route' => isset($item['badge_header_route'])?$item['badge_header_route']:'#',
                   'badge_content' => $item['badge_content'], 'file_list' => isset($item['file_list'])?$item['file_list']:null, 'date' => isset($item['date'])?$item['date']:null
                    ]
         )
