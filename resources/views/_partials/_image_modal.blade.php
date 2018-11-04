8d20fa4e1b6966ce14f0c05c322e9c70686b50ee
santius0 <santius1995@gmail.com>
2018-11-03 22:28:34 -0400

    finishing timeline

--- resources/views/_partials/_image_modal.blade.php
+++ resources/views/_partials/_image_modal.blade.php
@@ -1,24 +1,22 @@
+@php($first = true )
 <div class="modal" id="{{ $id }}" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
-                        @if(isset($images))
-                        <div class="item active">
-                            <img class="img-responsive" src="http://placehold.it/1200x600/fffccc/000&text=One" alt="...">
-                            <div class="carousel-caption">
-                                One Image
-                            </div>
-                        </div>
                         @foreach($images as $image)
-                            <div class="item">
-                                <img class="img-responsive" src="http://placehold.it/1200x600/fffccc/000&text=Two" alt="...">
+                            @if($first)
+                                <div class="item active">
+                                    @php($first = false )
+                            @else
+                                        <div class="item">
+                                            @endif
+                                <img class="img-responsive" src="http://placehold.it/1200x600/fffccc/000&text=One" alt="...">
                                 <div class="carousel-caption">
-                                    Another Image
+                                    One Image
                                 </div>
                             </div>
                         @endforeach
-                            @endif
                     </div>
                 <!-- Wrapper for slides -->
                 <!-- Controls -->
