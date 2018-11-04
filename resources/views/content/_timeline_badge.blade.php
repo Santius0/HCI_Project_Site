cdf25c852b217b11fb07e50f85d11f029a5eb263
santius0 <santius1995@gmail.com>
2018-11-03 23:33:24 -0400

    finishing timeline

--- resources/views/content/_timeline_badge.blade.php
+++ resources/views/content/_timeline_badge.blade.php
@@ -4,7 +4,7 @@
     <li class="timeline-inverted">
 @endif
         @if(isset($images))
-            <a href="#" data-toggle="modal" data-target="#imageCarousel">
+            <a href="https://www.google.com" data-toggle="modal" data-target="#imageCarousel">
                 <div class="timeline-badge">
                         <a><i class="fa fa-circle" id=""></i></a>
                 </div>
@@ -29,33 +29,33 @@
                             {{ isset($date) ? $date : '' }}
                         </div>
                     </div>
-                    </a>
+            </a>
                     @include('_partials._image_modal', ['id' => 'imageCarousel', 'images' => $images ])
-            @else
-                <div class="timeline-badge">
-                    <a><i class="fa fa-circle" id=""></i></a>
+        @else
+            <div class="timeline-badge">
+                <a><i class="fa fa-circle" id=""></i></a>
+            </div>
+            <div class="timeline-panel">
+                <div class="timeline-heading">
+                    <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
                 </div>
-                <div class="timeline-panel">
-                    <div class="timeline-heading">
-                        <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
-                    </div>
-                    <div class="timeline-body">
-                        <p> {{ $badge_content }}</p>
-                        @if(isset($file_list))
-                            <div class="files">
-                                <ul class="file-list">
-                                    @foreach($file_list as $file)
-                                        <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                        <br>
-                    </div>
-                    <div class="timeline-footer">
-                        {{ isset($date) ? $date : '' }}
-                    </div>
+                <div class="timeline-body">
+                    <p> {{ $badge_content }}</p>
+                    @if(isset($file_list))
+                        <div class="files">
+                            <ul class="file-list">
+                                @foreach($file_list as $file)
+                                    <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <br>
                 </div>
-            @endif
-        </li>
+                <div class="timeline-footer">
+                    {{ isset($date) ? $date : '' }}
+                </div>
+            </div>
+        @endif
+    </li>
     <li class="clearfix no-float"></li>
