6df0d5d901a701c3ec927d3a54cd2decba71d8c1
santius0 <santius1995@gmail.com>
2018-11-03 23:39:15 -0400

    finishing timeline

--- resources/views/content/_timeline_badge.blade.php
+++ resources/views/content/_timeline_badge.blade.php
@@ -3,59 +3,33 @@
 @else
     <li class="timeline-inverted">
 @endif
-        @if(isset($images))
-            <a href="https://www.google.com" data-toggle="modal" data-target="#imageCarousel">
-                <div class="timeline-badge">
-                        <a><i class="fa fa-circle" id=""></i></a>
-                </div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
-                        </div>
-                        <div class="timeline-body">
-                            <p> {{ $badge_content }}</p>
-                            @if(isset($file_list))
-                                <div class="files">
-                                    <ul class="file-list">
-                                        @foreach($file_list as $file)
-                                            <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
-                                        @endforeach
-                                    </ul>
-                                </div>
-                            @endif
-                            <br>
-                        </div>
-                        <div class="timeline-footer">
-                            {{ isset($date) ? $date : '' }}
-                        </div>
+    <div class="timeline-badge">
+            <a><i class="fa fa-circle" id=""></i></a>
+    </div>
+        <div class="timeline-panel">
+            <div class="timeline-heading">
+                <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
+            </div>
+            <div class="timeline-body">
+                <p> {{ $badge_content }}</p>
+                @if(isset($file_list))
+                    <div class="files">
+                        <ul class="file-list">
+                            @foreach($file_list as $file)
+                                <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
+                            @endforeach
+                        </ul>
                     </div>
-            </a>
+                @endif
+                @if(isset($images))
+                    <a href="#" data-toggle="modal" data-target="#imageCarousel">Images</a>
                     @include('_partials._image_modal', ['id' => 'imageCarousel', 'images' => $images ])
-        @else
-            <div class="timeline-badge">
-                <a><i class="fa fa-circle" id=""></i></a>
+                @endif
+                <br>
             </div>
-            <div class="timeline-panel">
-                <div class="timeline-heading">
-                    <h4><a href="{{ route($badge_header_route) }}">{{ $badge_header }}</a></h4>
-                </div>
-                <div class="timeline-body">
-                    <p> {{ $badge_content }}</p>
-                    @if(isset($file_list))
-                        <div class="files">
-                            <ul class="file-list">
-                                @foreach($file_list as $file)
-                                    <li><a href="{{ $file['link'] }}" class="video-link">{{ $file['name'] }}</a></li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    <br>
-                </div>
-                <div class="timeline-footer">
-                    {{ isset($date) ? $date : '' }}
-                </div>
+            <div class="timeline-footer">
+                {{ isset($date) ? $date : '' }}
             </div>
-        @endif
+        </div>
     </li>
     <li class="clearfix no-float"></li>
