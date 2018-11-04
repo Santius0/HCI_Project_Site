6aab0d16dfe212a81f2e81f69dff84c5e4a3f0e4
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-04 05:52:47 -0400

    timeline

--- resources/views/content/prototype_overview.blade.php
+++ resources/views/content/prototype_overview.blade.php
@@ -7,19 +7,18 @@
     <br>
     <h1>Final LoFi Prototype</h1>
     <hr>
-    {{--<div class="jumbotron text-center">--}}
     <div class="text-center">
         <div class="row">
             <div class="flex-column">
-                <img src="{{ asset('images/prototypes/lofi/lofi_1.jpg') }}" title='LoFi_1' width="500", height="800">
-                <img src="{{ asset('images/prototypes/lofi/lofi_2.jpg') }}" title='LoFi_2' width="500", height="800">
-                <img src="{{ asset('images/prototypes/lofi/lofi_3.jpg') }}" title='LoFi_3' width="500", height="800">
-                <img src="{{ asset('images/prototypes/lofi/lofi_4.jpg') }}" title='LoFi_4' width="500", height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_1.jpg') }}" title='LoFi_1' width="500" height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_2.jpg') }}" title='LoFi_2' width="500" height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_3.jpg') }}" title='LoFi_3' width="500" height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_4.jpg') }}" title='LoFi_4' width="500" height="800">
             </div>
             <div class="flex-column">
-                <img src="{{ asset('images/prototypes/lofi/lofi_5.jpg') }}" title='LoFi_5' width="500", height="800">
-                <img src="{{ asset('images/prototypes/lofi/lofi_6.jpg') }}" title='LoFi_6' width="500", height="800">
-                <img src="{{ asset('images/prototypes/lofi/lofi_7.jpg') }}" title='LoFi_7' width="500", height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_5.jpg') }}" title='LoFi_5' width="500" height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_6.jpg') }}" title='LoFi_6' width="500" height="800">
+                <img src="{{ asset('images/prototypes/lofi/lofi_7.jpg') }}" title='LoFi_7' width="500" height="800">
             </div>
         </div>
     </div>
