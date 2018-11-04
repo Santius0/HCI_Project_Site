8ee8af5e6fc4ad8d9a3c8dd239a0955a1a440309
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-04 03:44:09 -0400

    timeline

--- resources/views/_partials/_image_modal.blade.php
+++ resources/views/_partials/_image_modal.blade.php
@@ -5,18 +5,17 @@
             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         @foreach($images as $image)
-{{--                            @php($src = isset($image['asset_src']) ? $image['asset_src'] : $image['src'])--}}
                             @if($first)
                                 @php($first = false)
                                 <div class="item active">
-                                    <img class="img-responsive" src="{{ asset('images/Users/1.jpg') }}" alt= "{{ $image['name'] }}">
+                                    <img class="img-responsive" src="{{ $image['src'] }}" alt= "{{ $image['name'] }}">
                                     <div class="carousel-caption">
                                         {{ $image['name'] }}
                                     </div>
                                 </div>
                             @else
                                 <div class="item">
-                                    <img class="img-responsive" src="{{ asset('images/Users/1.jpg') }}" alt= "{{ $image['name'] }}">
+                                    <img class="img-responsive" src="{{ $image['src'] }}" alt= "{{ $image['name'] }}">
                                     <div class="carousel-caption">
                                         {{ $image['name'] }}
                                     </div>
