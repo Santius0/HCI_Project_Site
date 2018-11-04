6a7ac2fe797f6bdc59fc9874f4dbd78f83e5cae7
santius0 <santius1995@gmail.com>
2018-11-03 21:15:55 -0400

    finishing timeline

--- resources/views/_partials/_image_modal.blade.php
+++ resources/views/_partials/_image_modal.blade.php
@@ -1,7 +1,39 @@
 <div class="modal" id="{{ $id }}" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <button type="button" class="close pull-right" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img class="img-responsive" src="http://placehold.it/1200x600/555/000&text=One" alt="...">
+                        <div class="carousel-caption">
+                            One Image
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="http://placehold.it/1200x600/fffccc/000&text=Two" alt="...">
+                        <div class="carousel-caption">
+                            Another Image
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img class="img-responsive" src="http://placehold.it/1200x600/fcf00c/000&text=Three" alt="...">
+                        <div class="carousel-caption">
+                            Another Image
+                        </div>
+                    </div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+            </div>
 
         </div>
     </div>
 </div>
+
