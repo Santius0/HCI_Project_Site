7053d2d914dd5b856942537e40c1dcc24435a0c8
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-04 11:57:50 -0400

    removed debug

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,13 +1,12 @@
 @title('Something went wrong')
 
-@extends('layouts.base', ['disableAds' => true])
+@extends('layouts.base')
 
 @section('body')
     <div class="jumbotron text-center">
         <h1>{{ $title }}</h1>
         <p>
             We've been notified and will try to fix the problem as soon as possible.<br>
-            Please <a href="https://github.com/laravelio/portal/issues/new">open a Github issue</a> if the problem persists.
         </p>
     </div>
 @endsection
