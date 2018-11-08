f404190c0cbb8bb7fefa0338cb321579f4733eb0
santius0 <santius1995@gmail.com>
2018-11-08 17:49:58 -0400

    prototyping to design

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -27,17 +27,17 @@
                 <li class="dropdown {{ active(['research', 'research*']) }}">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Research <span class="caret"></span></a>
                     <ul class="dropdown-menu">
+                        {{--<li><a href="{{ route('research') }}">Overview</a></li>--}}
                         <li><a href="{{ route('research.methods') }}">Research Methods</a></li>
                         <li><a href="{{ route('research.results') }}">Research Results</a></li>
                     </ul>
                 </li>
 
                 <li class="dropdown {{ active(['prototype', 'prototype*']) }}">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Application Design <span class="caret"></span></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Prototyping <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="{{ route('prototype.testing') }}">User Testing</a></li>
                         <li><a href="{{ route('prototype') }}">LoFi Prototype</a></li>
-{{--                        <li><a href="{{ route('prototype.timeline') }}">Application Timeline</a></li>--}}
+                        <li><a href="{{ route('prototype.timeline') }}">Application Timeline</a></li>
                     </ul>
                 </li>
 
