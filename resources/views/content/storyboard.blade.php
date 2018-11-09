42957c6dea39b908262584d2fc950aaa438a8477
santius0 <santius1995@gmail.com>
2018-11-09 00:32:12 -0400

    added youtube video page

--- resources/views/content/storyboard.blade.php
+++ resources/views/content/storyboard.blade.php
@@ -7,12 +7,27 @@
 
     </p>
 
-    <div class="story">
-        <div class="story-panel">
-                fgfg
+    <article class="comic">
+        <div class="panel">
+            <p class="text top-left">Suddenly...</p>
+            <p class="text bottom-right">...something amazing happened</p>
         </div>
-        gfgfgf
-    </div>
+        <div class="panel">
+            <p class="text top-left">Try resizing...</p>
+            <p class="text bottom-right">...it's responsive</p>
+        </div>
+        <div class="panel">
+            <p class="speech">A speech bubble</p>
+        </div>
+        <div class="panel"></div>
+        <div class="panel"></div>
+        <div class="panel"></div>
+        <div class="panel"></div>
+        <div class="panel"></div>
+        <div class="panel">
+            <p class="text bottom-right">THE END</p>
+        </div>
+    </article>
     <div class="media">
         <h4>Video Prototype</h4>
         <div class="media-body" style="text-align: center">
