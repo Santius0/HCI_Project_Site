3a0738f4e19820e31b8ba3b320c171b0edb47f4e
santius0 <santius1995@gmail.com>
2018-11-08 23:06:34 -0400

    added youtube video page

--- resources/views/content/storyboard.blade.php
+++ resources/views/content/storyboard.blade.php
@@ -4,7 +4,7 @@
 @section('post_body')
     <h4>Video Prototype</h4>
     <div class="media">
-        <div class="media-body">
+        <div class="media-body" style="text-align: center">
             {!! Embed::make("https://www.youtube.com/watch?v=fB8TyLTD7EE")->parseUrl()->setAttribute(['width' => 1000])->getIframe() !!}
         </div>
     </div>
