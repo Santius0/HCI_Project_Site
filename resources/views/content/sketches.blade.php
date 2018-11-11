542e910a0f9e39058f24ca9c3bec13f67fc32af6
santius0 <santius1995@gmail.com>
2018-11-11 10:11:12 -0400

    afinity images

--- resources/views/content/sketches.blade.php
+++ resources/views/content/sketches.blade.php
@@ -48,13 +48,11 @@
             <img src="{{ asset('images/Affinity Diagram/3.jpg') }}">
         </div>
         <div class="flex-column">
-            <img src="{{ asset('images/Affinity Diagram/4.jpg') }}" width="50%" height="650">
-            <img src="{{ asset('images/Affinity Diagram/5.jpg') }}" width="50%" height="650">
-        </div>
-        <div class="flex-column">
-            <img src="{{ asset('images/Affinity Diagram/6.jpg') }}" width="50%" height="650">
-            <img src="{{ asset('images/Affinity Diagram/7.jpg') }}" width="50%" height="650">
-            <img src="{{ asset('images/Affinity Diagram/8.jpg') }}" width="50%" height="650">
+            <img src="{{ asset('images/Affinity Diagram/4.jpg') }}" width="200" height="450">
+            <img src="{{ asset('images/Affinity Diagram/5.jpg') }}" width="200" height="650">
+            <img src="{{ asset('images/Affinity Diagram/6.jpg') }}" width="200" height="450">
+            <img src="{{ asset('images/Affinity Diagram/7.jpg') }}" width="200" height="450">
+            <img src="{{ asset('images/Affinity Diagram/8.jpg') }}" width="200" height="450">
         </div>
     </div>
 @endsection
