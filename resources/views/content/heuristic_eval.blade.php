d2bc44a6c63e73324d4f81db22c29ad7db1d599e
santius0 <santius1995@gmail.com>
2018-11-16 20:34:45 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -7,7 +7,7 @@
            Tasks
        </div>
        <div class="inline" style="width: 33%; height: 850px;">
-           <div class="protoio-embed-prototype" data-code="GZTL1G" data-show-sidebuttons="0" style="width: 33%; height: 800px;"></div>
+           <div class="protoio-embed-prototype" data-code="GZTL1G" data-show-sidebuttons="0" style="width: 100%; height: 800px;"></div>
            <script async src="https://static.proto.io/api/widget-embed.js"></script>
        </div>
        <div class="inline" style="width: 33%; height: 800px;">
