4d10cd132837f1d2d6cedd628f8193fa54f3f965
santius0 <santius1995@gmail.com>
2018-11-17 01:05:27 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -3,7 +3,18 @@
 @php($heuristics = [])
 @section('post_body')
     <div id="evaluation">
-        {{--@foreach($heuristics as $heuristic)--}}
+        @foreach($heuristics as $heuristic)
+        <div class="timeline-panel">
+            <div class="timeline-heading">
+                <h4>{{ $heuristic['title'] }}</h4>
+            </div>
+            <div class="timeline-body">
+                <p> {{ $heuristic['description'] }}</p>
+                <br>
+            </div>
+            <div class="timeline-footer">
+            </div>
+        </div>
             <div class="container">
                 <p>
                     <label for="new-task">Add Item</label><input id="new-task" type="text"><button>Add</button>
@@ -21,7 +32,7 @@
                     <li><input type="checkbox" checked><label>See the Doctor</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
                 </ul>
             </div>
-        {{--@endforeach--}}
+        @endforeach
     </div>
     <div>
         Results
