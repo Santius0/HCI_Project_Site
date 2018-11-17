c5c12d48d988e6963964f2fb0b9992bad4078d92
santius0 <santius1995@gmail.com>
2018-11-17 10:00:37 -0400

    added vertical prototype

---
+++ resources/views/content/hifi.blade.php
@@ -0,0 +1,44 @@
+@extends('content.base')
+@title('Vertical Prototype')
+
+@section('post_body')
+    <br/>
+    <div class="tree">
+        <ul>
+            <li>
+                <a href="#">Parent</a>
+                <ul>
+                    <li>
+                        <a href="#">Child</a>
+                        <ul>
+                            <li>
+                                <a href="#">Grand Child</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Child</a>
+                        <ul>
+                            <li><a href="#">Grand Child</a></li>
+                            <li>
+                                <a href="#">Grand Child</a>
+                                <ul>
+                                    <li>
+                                        <a href="#">Great Grand Child</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Great Grand Child</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Great Grand Child</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Grand Child</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+@endsection
