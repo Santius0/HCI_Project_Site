4d56795cfca2e61a94faeeac9733a65d6390fa4c
santius0 <santius1995@gmail.com>
2018-11-17 01:14:32 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -13,25 +13,19 @@
                 <br>
             </div>
             <div class="timeline-footer">
+                {!! Form::open(['route' => $route, 'method' => $method ?? 'POST']) !!}
+
+                @formGroup('body')
+                {!! Form::label('Details') !!}
+                @include('layouts.editor')
+                @error('body')
+                @endFormGroup
+
+                {!! Form::submit(isset($thread) ? 'Update Thread' : 'Create Thread', ['class' => 'btn btn-primary btn-block']) !!}
+                <a href="{{ isset($thread) ? route('thread.show', $thread->id()) : route('threads') }}" class="btn btn-default btn-block">Cancel</a>
+                {!! Form::close() !!}
             </div>
         </div>
-            <div class="container">
-                <p>
-                    <label for="new-task">Add Item</label><input id="new-task" type="text"><button>Add</button>
-                </p>
-
-                <h3>Todo</h3>
-                <ul id="incomplete-tasks">
-                    <li><input type="checkbox"><label>Pay Bills</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-                    <li class="editMode"><input type="checkbox"><label>Go Shopping</label><input type="text" value="Go Shopping"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-
-                </ul>
-
-                <h3>Completed</h3>
-                <ul id="completed-tasks">
-                    <li><input type="checkbox" checked><label>See the Doctor</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-                </ul>
-            </div>
         @endforeach
     </div>
     <div>
