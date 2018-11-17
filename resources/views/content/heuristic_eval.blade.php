b3a333b7553f427a41b4edffa2346ddfaa854a85
santius0 <santius1995@gmail.com>
2018-11-17 01:59:19 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -13,7 +13,8 @@
                     {!! Form::hidden('title', $heuristic['title'], ['class' => 'form-control', 'required', 'maxlength' => '60']) !!}
                     <span class="help-block">{!! $heuristic['description'] !!}}</span>
                     @error('subject')
-                    @endFormGroup                </div>
+                    @endFormGroup
+                </div>
                 <div class="timeline-body">
                     @endFormGroup @formGroup('body')
                     {!! Form::label('Details') !!}
@@ -22,9 +23,43 @@
                     @endFormGroup
                 </div>
                 <div class="timeline-footer">
-                    {!! Form::submit('Submit', ['class' => 'btn btn-primary btn-block']) !!}
-                    {!! Form::close() !!}
+                    <div class="form-radio form-radio-inline">
+                        <div class="form-radio-legend">Prefered Pronoun</div>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="He" />
+                            <i class="form-radio-button"></i>
+                            <span>He</span>
+                        </label>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="She" />
+                            <i class="form-radio-button"></i>
+                            <span>She</span>
+                        </label>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="They" />
+                            <i class="form-radio-button"></i>
+                            <span>They</span>
+                        </label>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="Ze" />
+                            <i class="form-radio-button"></i>
+                            <span>Ze</span>
+                        </label>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="A pronoun not listed" />
+                            <i class="form-radio-button"></i>
+                            <span>A pronoun not listed</span>
+                        </label>
+                        <label class="form-radio-label">
+                            <input name=pronoun class="form-radio-field" type="radio" required value="No pronoun preference" />
+                            <i class="form-radio-button"></i>
+                            <span>No pronoun preference</span>
+                        </label>
+                        <small class="form-element-hint">Feel free to choose</small>
+                    </div>
                 </div>
+                {!! Form::submit('Submit', ['class' => 'btn btn-primary btn-block']) !!}
+                {!! Form::close() !!}
             </div>
             <hr><br>
         @endforeach
