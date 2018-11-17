ad82d76e710d17edee2467a76fa6cbf12907877e
santius0 <santius1995@gmail.com>
2018-11-16 20:30:50 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -3,14 +3,12 @@
 
 @section('post_body')
    <div id="evaluation">
-       <div class="inline">
+       <div class="inline" style="width: 33%; height: 800px;">
            Tasks
        </div>
-       <div class="inline">
-           <div class="protoio-embed-prototype" data-code="GZTL1G" data-show-sidebuttons="0" style="width: 100%; height: 800px;"></div>
+           <div class="inline protoio-embed-prototype" data-code="GZTL1G" data-show-sidebuttons="0" style="width: 33%; height: 800px;"></div>
            <script async src="https://static.proto.io/api/widget-embed.js"></script>
-       </div>
-       <div class="inline">
+       <div class="inline" style="width: 33%; height: 800px;">
            Problems
        </div>
    </div>
