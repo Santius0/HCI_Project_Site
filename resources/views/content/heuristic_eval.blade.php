2ca6acc135582416790edc44ca20baf27921e625
santius0 <santius1995@gmail.com>
2018-11-16 21:29:36 -0400

    added a4 pages

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -14,10 +14,12 @@
            <div class="protoio-embed-prototype" data-code="GZTL1G" data-show-sidebuttons="0" style="width: 100%; height: 800px;"></div>
            <script async src="https://static.proto.io/api/widget-embed.js"></script>
        </div>
-       <div class="inline" style="width: 33%; height: 800px;">
-           @foreach($tasks as $task)
-               <li><p><b> {{ $task }}</b></p></li>
-           @endforeach
+       <div class="w3-container inline" id="docs-list">
+           <ul id="myUL" class="w3-margin-top w3-margin-bottom">
+               @foreach($tasks as $task)
+                   <li><p><b> {{ $task }}</b></p></li>
+               @endforeach
+           </ul>
        </div>
    </div>
    <br>
