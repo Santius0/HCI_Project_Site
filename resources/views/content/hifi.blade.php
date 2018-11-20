f581ec5173bc09f0d4a2157fdecfd9cf976b3ca9
santius0 <santius1995@gmail.com>
2018-11-19 22:50:56 -0400

    vertical prototype

--- resources/views/content/hifi.blade.php
+++ resources/views/content/hifi.blade.php
@@ -5,11 +5,24 @@
     <br/>
     <h3>See <a href="{{asset('Prototype/index.html')}}" target="_blank">HiFi Demo</a> </h3><hr><br>
     <h3>Vertical Prototype</h3>
-    @foreach($systems as $system)
-    <div class="inline">
-        <div class="tree">
-            @include('content.vertical_tree_child', ['child' => $system['head']])
-        </div>
+    {{--@foreach($systems as $system)--}}
+    {{--<div class="inline">--}}
+        {{--<div class="tree">--}}
+            {{--@include('content.vertical_tree_child', ['child' => $system['head']])--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--@endforeach--}}
+    <div id="vertical-prototype">
+        <table>
+            <tr>
+                <th>Newsfeed</th>
+                <th>Alerts</th>
+                <th>Timetable</th>
+                <th>Campus Status</th>
+                <th>Shuttle Services</th>
+                <th>Campus Maps</th>
+                <th>Setting</th>
+            </tr>
+        </table>
     </div>
-    @endforeach
 @endsection
