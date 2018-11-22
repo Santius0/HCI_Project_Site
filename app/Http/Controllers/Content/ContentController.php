51859a4eca0c093f202964a0cdebc1157cc92c62
santius0 <santius1995@gmail.com>
2018-11-22 11:48:14 -0400

    prototype

--- app/Http/Controllers/Content/ContentController.php
+++ app/Http/Controllers/Content/ContentController.php
@@ -81,7 +81,7 @@ class ContentController extends Controller
     public function files()
     {
         $files = [
-          ['name' => 'Project Proposal Document', 'link' => asset('Project_Proposal_A1.pdf')],
+          ['name' => 'Project Proposal Document', 'link' => 'https://docs.google.com/document/d/1TR90babwad7q6gaxcaNVx403_kr3vhGTLZ9gc9swmzA/edit'],
         ];
         return view('content.files', compact('files'));
     }
