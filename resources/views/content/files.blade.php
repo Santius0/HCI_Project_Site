68f88b281ba2bf600ba172f87ce4d43c5ab2e052
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-22 14:12:24 -0400

    files

--- resources/views/content/files.blade.php
+++ resources/views/content/files.blade.php
@@ -5,6 +5,11 @@
     <br/>
     <h3>All documents and/or files created during this project can be found below.</h3>
     <br><hr>
+    <div class="alert alert-info">
+        <p>
+            <a href="{{ asset('files_and_docs/All_Project_File_HCI.zip') }}">Download All Files</a>
+        </p>
+    </div>
     <div class="w3-container" id="docs-list">
         <ul id="myUL" class="w3-margin-top w3-margin-bottom">
             @foreach($files as $file)
