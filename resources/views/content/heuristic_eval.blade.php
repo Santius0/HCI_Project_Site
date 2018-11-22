11ffd2216e71d7dfd06f0c97e1da8cd052186cf2
santius0 <santius1995@gmail.com>
2018-11-22 11:08:59 -0400

    prototype

--- resources/views/content/heuristic_eval.blade.php
+++ resources/views/content/heuristic_eval.blade.php
@@ -36,7 +36,7 @@
                         @endforeach
                             <div class="timeline-footer">
                                 <p>
-                                    Average Rating: {{ $rating/count($heuristic['responses']) }}
+                                    Average Rating: {{ round($rating/count($heuristic['responses']),2) }}
                                 </p>
                             </div>
                     @else
@@ -51,12 +51,8 @@
     <div>
         <h3>Evaluation Analysis</h3>
         <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-            veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-            consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-            velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem
-            ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-            molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+            The application's Heuristic Evaluation revealed small violations of Jakob's 10 heuristics, with the most pressing regarding Consistency and Standard as well as Help and Documentation.
+            Other violations were noted, however most of these were viewed as necessary or negligible in the context of the application. Adjustments were made to application in to correct these issues where possible and further changes will be made in future design iterations.
         </p>
     </div>
 @endsection
