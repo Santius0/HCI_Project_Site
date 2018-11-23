cec2242343822f43eecd35e87cd95821f6b8d6ef
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-22 21:47:39 -0400

    lots of changes

--- resources/views/content/hifi.blade.php
+++ resources/views/content/hifi.blade.php
@@ -10,8 +10,9 @@
         {{--</div>--}}
     {{--</div>--}}
     {{--@endforeach--}}
+    <p>The results of the heuristic analysis were used to update the HiFi Prototype.</p>
+    <h4><a href="{{asset('HiFi_Prototype/HiFi_Prototype_1.1/index.html')}}" target="_blank">HiFi Prototype 1.1</a></h4>
     <h2>Prototype Layout</h2>
-    <p>The results of the heuristic analysis were used to update the HiFi Prototype. The newly updated prototype can be found <a href="{{asset('HiFi_Prototype/HiFi_Prototype_1.1/index.html')}}" target="_blank">Here</a></p>
     <br>
     <p>The following is an overview of the HiFi prototype.</p>
     <div id="vertical-prototype">
