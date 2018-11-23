8b9ae99e3279d877b4b68333d641c9dc2e5be877
Sergio Mathurin <santius.mathurin@yahoo.com>
2018-11-22 23:58:14 -0400

    lots of changes

--- resources/views/content/research_results.blade.php
+++ resources/views/content/research_results.blade.php
@@ -182,98 +182,104 @@
         </ul>
     </div>
     <br>
-    <h4><b>Results:</b></h4>
-    <h4><b>Observation 1</b></h4>
-    <h4><b>Random Scenario: </b></h4>
-    <table style="border: 1px solid black; border-collapse: collapse;">
-        <tr>
-            <th>Task #</th>
-            <th>Duration</th>
-            <th>Observations</th>
-            <th>Comments</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1 minute</td>
-            <td>Performed tasks expected. Additional Tasks performed were : Checking Alerts and Timetable</td>
-            <td>User hesitated but understood the layout right away.</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>1 minute</td>
-            <td>Performed task expected</td>
-            <td>User hesitated and reviewed the scenario statement again. However task was completed and they showed signs of knowing the task was completed before handing over the next task as a sign of completion.</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>30 seconds</td>
-            <td>Swiftly perform task as layout was fully grasped. User went to Campus Status and UWI maps. Showing that they understood the functionality of both.</td>
-            <td>User is becoming confident in actions.</td>
-        </tr>
-        <tr>
-            <td>(Randomly added scenario)</td>
-            <td>40 seconds</td>
-            <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
-            <td>User confidence is high. Has full understanding of app Activities and features.</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>1 mintute</td>
-            <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
-            <td>User’s reaction to this task means that this Screen objective is unclear. Review is needed so the user knows to view the NewsFeed for this category of information.</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>20 seconds</td>
-            <td>User immediately went to campus Maps.</td>
-            <td></td>
-        </tr>
-    </table>
-    <br><br>
-    <h4><b>Observation 2</b></h4>
-    <h4><b>Random Scenario: In the last lecture, you remembered the lecturer saying classes would be held in another venue.</b></h4>
-    <table style="border: 1px solid black; border-collapse: collapse;">
-        <tr>
-            <th>Task #</th>
-            <th>Duration</th>
-            <th>Observations</th>
-            <th>Comments</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>1 minute</td>
-            <td>Performed tasks expected. Additional Tasks performed were : Checking Alerts and Timetable</td>
-            <td>User hesitated but understood the layout right away.</td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>1 minute</td>
-            <td>Performed task expected</td>
-            <td>User hesitated and reviewed the scenario statement again. However task was completed and they showed signs of knowing the task was completed before handing over the next task as a sign of completion.</td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>30 seconds</td>
-            <td>Swiftly perform task as layout was fully grasped. User went to Campus Status and UWI maps. Showing that they understood the functionality of both.</td>
-            <td>User is becoming confident in actions.</td>
-        </tr>
-        <tr>
-            <td>(Randomly added scenario)</td>
-            <td>40 seconds</td>
-            <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
-            <td>User confidence is high. Has full understanding of app Activities and features.</td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>1 mintute</td>
-            <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
-            <td>User’s reaction to this task means that this Screen objective is unclear. Review is needed so the user knows to view the NewsFeed for this category of information.</td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>20 seconds</td>
-            <td>User immediately went to campus Maps.</td>
-            <td></td>
-        </tr>
-    </table>
+    <div id="observation-results">
+        <h4><b>Results:</b></h4>
+        <h4><b>Observation 1</b></h4>
+        <h4><b>Random Scenario: Find an available parking spot in UWI.</b></h4>
+        <table>
+            <tr>
+                <th>Task #</th>
+                <th>Duration</th>
+                <th>Observations</th>
+                <th>Comments</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>1 minute</td>
+                <td>Performed tasks expected. Additional Tasks performed were : Checking Alerts and Timetable</td>
+                <td>User hesitated but understood the layout right away.</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>1 minute</td>
+                <td>Performed task expected</td>
+                <td>User hesitated and reviewed the scenario statement again. However task was completed and they showed signs of knowing the task was completed before handing over the next task as a sign of completion.</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>30 seconds</td>
+                <td>Swiftly perform task as layout was fully grasped. User went to Campus Status and UWI maps. Showing that they understood the functionality of both.</td>
+                <td>User is becoming confident in actions.</td>
+            </tr>
+            <tr>
+                <td>(Randomly added scenario)</td>
+                <td>40 seconds</td>
+                <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
+                <td>User confidence is high. Has full understanding of app Activities and features.</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>1 mintute</td>
+                <td>The user went to Campus status to view available parking and also used the map stating that they can use it to locate that car park.</td>
+                <td>User’s reaction to this task means that this Screen objective is unclear. Review is needed so the user knows to view the NewsFeed for this category of information.</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>20 seconds</td>
+                <td>User immediately went to campus Maps.</td>
+                <td></td>
+            </tr>
+        </table>
+        <br><br>
+        <h4><b>Observation 2</b></h4>
+        <h4><b>Random Scenario: In the last lecture, you remembered the lecturer saying classes would be held in another venue.</b></h4>
+        <table style="border: 1px solid black; border-collapse: collapse;">
+            <tr>
+                <th>Task #</th>
+                <th>Duration</th>
+                <th>Observations</th>
+                <th>Comments</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>5 minutes</td>
+                <td>The User clicking on all the menu options available to assess the apps and its features.
+                    Task was completed eventually and comments on the interface were given.
+                </td>
+                <td>This different approach from this user shows that users study and appraise the usefulness and appeal of a product differently.</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>1 minute</td>
+                <td>Since the user dissected the app in the previous task. The  User has a comprehension of the features of the product.</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>30 seconds</td>
+                <td></td>
+                <td>The user made a request that the amount of shuttles currently active when the status is running is made available to users.</td>
+            </tr>
+            <tr>
+                <td>(Randomly added scenario)</td>
+                <td>20 minutes</td>
+                <td>The user was able perform the task, View timetable, after viewing Alerts.</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>1 minute</td>
+                <td>The user hesitated then proceeded to select campus status.
+                    The information to complete the task however was on newsfeed.
+                </td>
+                <td>Review on what services and facilities are displayed in Campus status. The inclusion status for the University’s site, Online payment, etc.</td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>10 seconds</td>
+                <td>User was comfortable with interface.</td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
 @endsection
